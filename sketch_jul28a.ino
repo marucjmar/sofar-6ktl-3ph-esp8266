e80cdf4305cdb9257afac60250349824e3ace878
Marcin Lazar <mlazar@inelo.pl>
2024-07-31 23:08:34 +0200

    refactoring

--- sketch_jul28a.ino
+++ sketch_jul28a.ino
@@ -21,13 +21,13 @@ void initWiFi() {
   Serial.println(WiFi.localIP());
 }
 
-void onWifiConnect(const WiFiEventStationModeGotIP& event) {
+void onWifiConnect(const WiFiEventStationModeGotIP &event) {
   Serial.println("Connected to Wi-Fi sucessfully.");
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
 }
 
-void onWifiDisconnect(const WiFiEventStationModeDisconnected& event) {
+void onWifiDisconnect(const WiFiEventStationModeDisconnected &event) {
   Serial.println("Disconnected from Wi-Fi, trying to connect...");
   WiFi.disconnect();
   WiFi.begin(ssid, password);
@@ -35,21 +35,21 @@ void onWifiDisconnect(const WiFiEventStationModeDisconnected& event) {
 
 void setup() {
   Serial.begin(115200);
-  
+
   pinMode(RELAY_PIN, OUTPUT);
   pinMode(LED_BUILTIN, OUTPUT);
 
-  //Register event handlers
+  // Register event handlers
   wifiConnectHandler = WiFi.onStationModeGotIP(onWifiConnect);
   wifiDisconnectHandler = WiFi.onStationModeDisconnected(onWifiDisconnect);
-   
+
   initWiFi();
   Serial.print("RRSI: ");
   Serial.println(WiFi.RSSI());
 }
 
-const int measurementInterval = 30 * 1000; // Czestotliwosc probkowania
-const int minPower = 4000; // Minimalna moc w watach po przekroczeniu której ma się uruchamiać grzalka
+const int measurementInterval = 30 * 1000;                           // Czestotliwosc probkowania
+const int minPower = 4000;                                           // Minimalna moc w watach po przekroczeniu której ma się uruchamiać grzalka
 const String serverStatusPath = "http://192.168.2.100/status.html"; // Adres url do falownika
 int lastProbeTime = 0;
 
@@ -57,8 +57,8 @@ void loop() {
   if ((millis() - lastProbeTime) > measurementInterval) {
     digitalWrite(LED_BUILTIN, HIGH);
 
-    //Check WiFi connection status
-    if(WiFi.status()== WL_CONNECTED){
+    // Check WiFi connection status
+    if (WiFi.status() == WL_CONNECTED) {
 
       int currentPower = getCurrentPower();
 
@@ -97,12 +97,12 @@ int getCurrentPower() {
 int doGetCurrentPower() {
   WiFiClient client;
   HTTPClient http;
-  
+
   http.begin(client, serverStatusPath.c_str());
   http.addHeader("Authorization", "Basic YWRtaW46YWRtaW4="); // basic auth: admin/admin
 
   const int httpResponseCode = http.GET();
-  
+
   if (httpResponseCode > 0) {
     const String payload = http.getString();
     const int value = extractCurrentPower(payload);
@@ -121,6 +121,11 @@ int extractCurrentPower(String str) {
   const String searchString = "var webdata_now_p = ";
   const int startIndex = str.indexOf(searchString) + searchString.length();
   const int endIndex = str.indexOf(';', startIndex);
+  const String val = str.substring(startIndex + 1, endIndex - 1);
 
-  return str.substring(startIndex + 1, endIndex -1).toInt() * 10; // fix na Moc w Watach, strona podaje wartość ze źle przesuniętym przecinkiem
+  if (isDigit(val.charAt(0))) { // wartość moze byc zwrocona jako ---
+    return val.toInt() * 10; // fix na Moc w Watach, strona podaje wartość ze źle przesuniętym przecinkiem
+  }
+
+  return -1;
 }
